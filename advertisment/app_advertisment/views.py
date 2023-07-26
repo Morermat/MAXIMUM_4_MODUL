14f1b404bcdfa88669be1fead39430f7f0b9f27b
Morermat <fadin666@mail.ru>
2023-07-26 18:22:40 +1000

    5 занятие

--- advertisment/app_advertisment/views.py
+++ advertisment/app_advertisment/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Первая строка')
+    return render(request, 'index.html')
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+def register(request):
+    return render(request, 'register.html')
+def profile(request):
+    return render(request, 'profile.html')
+def login(request):
+    return render(request, 'login.html')
+def advertisement(request):
+    return render(request, 'advertisement.html')
+def advertisement_post(request):
+    return render(request, 'advertisement_post.html')
